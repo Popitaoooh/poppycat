ea60103fcb04d97e7bae68d2b574644f3db46451
Popitaoooh <45242546+Popitaoooh@users.noreply.github.com>
2022-10-04 18:54:03 -0400

    Add files via upload

---
+++ build/basic1.js
@@ -0,0 +1,14 @@
+var trashval = 25
+var perclick = 1
+var delayInMilliseconds = 3500;
+
+function btnClick(){
+	trashval --;
+	document.getElementById("trashval").innerHTML = trashval;
+	return true 
+
+setTimeout(function() {
+}, delayInMilliseconds);
+window.location.href = "spooky.html";
+return true 
+}
